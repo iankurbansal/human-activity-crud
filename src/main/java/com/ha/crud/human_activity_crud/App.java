7fcb92a6814f5e772aad1c5717946a7d8faac800
Ankur Bansal <ankur.a.bansal@ericsson.com>
2019-03-31 13:46:25 +0100

    Initial commit

---
+++ src/main/java/com/ha/crud/human_activity_crud/App.java
@@ -0,0 +1,13 @@
+package com.ha.crud.human_activity_crud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
